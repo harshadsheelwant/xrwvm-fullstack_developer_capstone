d850e4ed8c03a165d37b5156a4245886105bae9d
Harshad <harshadsheelwant1@gmail.com>
2026-04-13 17:23:23 +0530

    Second last task

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,22 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path('login', views.login_user, name='login'),
+    # path for logout
+    path('logout', views.logout_request, name='logout'),
+
+    # path for registration
+    path('registration', views.registration, name='registration'),
 
     # path for dealer reviews view
+    # path('dealer/<int:dealer_id>/reviews', views.get_dealer_reviews, name='dealer_reviews'),
 
     # path for add a review view
+    # path('dealer/<int:dealer_id>/add_review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
